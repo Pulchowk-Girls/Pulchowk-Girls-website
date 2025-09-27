4eeb40a01ec69e1c8283c2b4c44f7acb9f6b38e6
Anupa Ranabhat <anupa.ranabhat@gmail.com>
2025-09-27 22:15:52 +0545

    updated ConatctUs section

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React from "react";
+import "../styles/ContactUs.css";
 
 function ContactUs() {
   return (
-    <div className="container">
-      <div className="container-header">
-        <h1>Contact Us</h1>
+    <div className="contact-us-container">
+      <img
+        src="/Logo.png"
+        alt="Logo"
+        style={{
+          height: "40px",
+          marginRight: "8px",
+          marginLeft: "60px",
+          verticalAlign: "middle",
+          marginBottom: "60px",
+        }}
+      />
+      <div className="contact-us-container-content">
+        <h5 style={{ marginBottom: "20px" }}>CONTACT US</h5>
+        <p>
+          +9779847630043 <br />
+          Pulchowk Campus, Lalitpur <br />
+          pulchowkgirls@pcampus.edu.np
+        </p>
       </div>
     </div>
-  )
+  );
 }
 
-export default ContactUs
+export default ContactUs;
